2002864da4d2b54030149df75d3147356d901946
HeitorBMarini <mariniheitor@gmail.com>
2025-11-16 12:19:31 -0300

    ajuste banner

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function Banner() {
   return (
-    <section className="relative w-full h-[60vh] md:h-[70vh] overflow-hidden">
+    <section className="relative w-full h-[60vh] md:h-[80vh] overflow-hidden">
       <Image
         src="/imgs/banner.webp"
         alt="Banner principal"
